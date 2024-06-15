e5f96e22221021a93276ca44528b485361a09398
NgocDangVan <ngocdangvan98@gmail.com>
2024-06-15 15:36:49 +0700

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+@NgModule({
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, FontAwesomeModule, AppRoutingModule,HomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
